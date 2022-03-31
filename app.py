d93a1109bde62bf6b27131cf0a710d57c0a374f4
Miro Frankovic <miro.frankovic@gmail.com>
2022-03-31 18:32:38 +0000

    Add isnumber function

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ board = [
 # take board as parameter
 # slot a variable
 
-def print_board(board):
+def print_board(board):  #board input parameter internal variable as passed in as parameter
     for row in board:
         for slot in row:
             print(f"{slot} ", end="")   
@@ -16,10 +16,21 @@ def print_board(board):
 print_board(board)
 
 
-def quit(usser_input):
-    if user_input == "q":reture True
+def quit(user_input):
+    if user_input.lower() == "q":
+        print("Thank you for playing")
+        return True
     else:return False
 
+def check_input(user_input):
+    isnum(user_input)
+
+def isnum(user_input):
+    if not user_input.isnumeric():
+        print("This is not valid number!")
+        return False
+    else: return True    
+
 while True:
-    user_input = input("Please enter the position from 1 to 9 or enter \"q\" to quit the game")
-    
+    user_input = input("Please enter the position from 1 to 9 or enter \"q\" to quit the game: ")
+    if quit(user_input):break
