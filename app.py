815c1629a9de602ffe434b5603568eaacf39a8ff
Miro Frankovic <miro.frankovic@gmail.com>
2022-07-17 11:46:14 +0000

    Create classes and functions for snake game

--- app.py
+++ app.py
@@ -1,36 +1,22 @@
-board = [
-    ["-","-","-"],
-    ["-","-","-"],
-    ["-","-","-"]   
-]
+from thinker import * 
+import random
 
-# take board as parameter
-# slot a variable
+class Snake:
+    pass
 
-def print_board(board):  #board input parameter internal variable as passed in as parameter
-    for row in board:
-        for slot in row:
-            print(f"{slot} ", end="")   
-        print()    
+class Food:
+    pass
 
-print_board(board)
+def next_turn():
+    pass
+
+def change_direction(new_direction):
+    pass
+
+def check_collisions():
+    pass
+
+def game_over():
+    pass
 
 
-def quit(user_input):
-    if user_input.lower() == "q":
-        print("Thank you for playing")
-        return True
-    else:return False
-
-def check_input(user_input):
-    isnum(user_input)
-
-def isnum(user_input):
-    if not user_input.isnumeric():
-        print("This is not valid number!")
-        return False
-    else: return True    
-
-while True:
-    user_input = input("Please enter the position from 1 to 9 or enter \"q\" to quit the game: ")
-    if quit(user_input):break
