46c111814fe1e9945d84bc20b459909c2961e2c4
Miro Frankovic <miro.frankovic@gmail.com>
2022-07-20 20:17:07 +0000

    Add constants

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from thinker import * 
 import random
 
+GAME_WIDTH = 800
+GAME_HEIGHT = 800
+SPEED = 30
+SPACE_SIZE = 40
+BODY_PARTS = 4
+SNAKE_COLOR = "green"
+FOOD_COLOR ="red"
+BACKGROUND_COLOR = "black"
+
+
 class Snake:
     pass
 
